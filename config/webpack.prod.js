f4d4cd854e347231a248b8949ecc11838d47ed32
Justas Česnauskas <justascesnauskas7@gmail.com>
2020-03-24 22:27:08 +0200

    fix: webpack env

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -105,7 +105,9 @@ module.exports = {
     }),
     new BrotliPlugin(),
     new webpack.DefinePlugin({
-      GOOGLE_DOCS: "X2we6Owl8jc4HYpNg2JXFjpI"
+      GOOGLE_DOCS: JSON.stringify(
+        "https://docs.google.com/spreadsheets/d/19UnTaiLiAo_X2we6Owl8jc4HYpNg2JXFjpI-j-qNCcA/gviz/tq?gid=0&headers=1&tq="
+      )
     })
   ]
 };
