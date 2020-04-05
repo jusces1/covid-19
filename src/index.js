31bbb32b0a8f16215cf793b3320a4827131d0bdf
Justas Česnauskas <justascesnauskas7@gmail.com>
2020-04-05 20:38:47 +0300

    fix: chart select update

--- src/index.js
+++ src/index.js
@@ -4,6 +4,28 @@ import axios from "axios";
 import "./styles/style.css";
 
 async function init() {
+  document.getElementById(
+    "covid-19-charts-container"
+  ).innerHTML = `<select id="states"></select>
+  <div class="chart-container">
+    <div class="chart">
+      <div class="linear-chart" id="linear-chart"></div>
+    </div>
+    <div class="chart">
+      <div class="column-char" id="column-chart"></div>
+    </div>
+  </div>
+  <div class="chart-container">
+    <div class="chart">
+      <div class="column-chart-testings" id="column-chart-testings"></div>
+    </div>
+    <div class="chart">
+      <div
+        class="column-chart-population-confirmed"
+        id="column-chart-population-confirmed"
+      ></div>
+    </div>
+  </div>`;
   const usHistData = await axios.get("https://covidtracking.com/api/us/daily");
   const stateHistData = await axios.get(
     "https://covidtracking.com/api/states/daily"
